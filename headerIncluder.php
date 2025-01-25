0e92c3c09ec273de1b3e618f2a9c1bea54a9e8f8
boyan hb <bobibonev1@abv.bg>
2025-01-25 02:29:16 +0200

    Adding the base structure and style of the dashboard and admin settings page alongside a potential php file for inclusion of universal header for all pages

---
+++ headerIncluder.php
@@ -0,0 +1,25 @@
+<?php
+// insert code here
+?>
+
+    <link rel="stylesheet" href="headerStyles.css">
+    <header>
+        <section id="profileInfo">
+            <!-- take the information from a database about users -->
+            <img id="profilePicture" alt = "N/A"></img>
+            <p id="profileName">Profile name</p>
+        </section>
+        <nav id="topNav">
+            <ul>
+                <!-- make admin settings and create event appear only if the user has the rights to it -->
+                <li><a href="adminSettings.html">Administrative Settings</a></li>
+                <li><a href="createEvent.html">Create Event</a></li>
+                <li><a href="profile.html">Profile</a></li>
+                <li><a href="login.html">Log out</a></li>
+            </ul>
+        </nav>
+    </header>
+
+<?php
+// insert code here
+?>
