5870b058f79686aa2b895edd59e98ae7c5e4dc94
Boyan-Bonev <bobibonev1@abv.bg>
2025-02-09 02:59:24 +0200

    Added the functionality behind activate command but is not yet connected to any database or server info, just client-side for now.

---
+++ event/activateCommand.js
@@ -0,0 +1,35 @@
+function activateSelectedCommand() {
+    const selectedCommand = document.getElementById("commandSelect").value;
+    const delay = parseInt(document.getElementById("delayInput").value);
+    const minPoints = parseInt(document.getElementById("pointsInput").value);
+
+    displayCommand(selectedCommand, delay);
+
+    closePopup('commandPopup');
+}
+
+function displayCommand(command, duration) {
+    const commandText = document.getElementById("commandText");
+    const countdownDisplay = document.getElementById("countdown");
+
+    commandText.textContent = command;
+
+    let timeLeft = duration;
+
+    if (timeLeft >= 0) {
+    countdownDisplay.textContent = timeLeft;
+    } else {
+    countdownDisplay.textContent = "Time's up!";
+    }
+
+    const countdownInterval = setInterval(() => {
+        timeLeft--;
+        countdownDisplay.textContent = timeLeft;
+
+        if (timeLeft < 0) {
+            clearInterval(countdownInterval);
+            commandText.textContent = "";
+            countdownDisplay.textContent = "";
+        }
+    }, 1000); // Update every second
+}
